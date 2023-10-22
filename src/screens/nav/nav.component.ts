00465d705504809d82db3e7e4dcfc523946b4f0c
Miguel Hinostroza Vera <mhinostrozave@intercorp.com.pe>
2023-10-21 22:33:58 -0500

    Initialize Project

---
+++ src/screens/nav/nav.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'mhp-nav',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.sass']
+})
+export class NavComponent {
+}
